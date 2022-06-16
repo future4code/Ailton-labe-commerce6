4d81fbeca6178f98a5d38c3991444d5b1738f2fc
Gabriel <102243306+GabeSales@users.noreply.github.com>
2022-06-16 09:43:24 -0300

    Revert "botão filtro que esconde"

--- src/components/Mayara/filtro.js
+++ src/components/Mayara/filtro.js
@@ -3,45 +3,14 @@ import styled from "styled-components";
 
 const Button = styled.button `
 background-color: greenyellow;
-color: black;
-padding: 10px;
-border-radius: 5px;
-`
-const Input = styled.input `
-  width: 150px;
-  padding: 12px 10px;
-  margin: 6px 0;
-  box-sizing: border-box;
-  border-radius: 5px;
+color: white;
 `
 
 
-  class Filtro extends React.Component {
-    render() {
-      return (
-        <div>
-          <div>
-            <p>
-          <form>
-            <label> Valor Mínimo: <p><Input type="number" min={0} name="minimo" placeholder="digite o valor"/> </p> </label>
-          </form>
-          </p>
-          <p>
-          <form>
-            <label> Valor Máximo: <p><Input type="number" max={1000} name="maximo" placeholder="digite o valor"/> </p> </label> 
-           </form> 
-           </p>
-           </div>
-        </div>
-      );
-    }
-  }
-  
-  
-  
-  export default class FiltroImportado extends React.Component {
+
+export class Filtro extends React.Component {
     state = {
-      more: false
+      more: true
     };
   
     mostraMore = () => {
@@ -53,12 +22,33 @@ const Input = styled.input `
     render() {
       return (
         <div>
-           
+      <div>
+            <h3>Filtro</h3>
+          </div>
+          <p>
+          <form>
+            <label>
+              Valor Mínimo: 
+              <input type="number" min={0} name="minimo" placeholder="Valor Mínimo"/>
+              </label>
+          </form>
+          </p>
+            <p>
+          <form>
+            <label>
+             Valor Máximo: 
+              <input type="number" name="máximo" max={1000} placeholder="Valor Máximo"/>
+            </label>
+          </form>
+            </p>
           <Button onClick={this.mostraMore}>
             {this.state.more ? "Esconder Filtro" : "Filtro"}
           </Button>
   
-          <div> {this.state.more && <Filtro />} </div>
+          <div>
+            {this.state.more && <Filtro />}
+            
+          </div>
         </div>
       );
     }
