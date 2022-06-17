db23b37640d8c9d9bb3cf3f8aa0ca2622ea4367c
Gabriel Sales <joaogabrsales@gmail.com>
2022-06-17 12:47:25 -0300

    atualização background e criação das pastas pra logica do carrinho

---
+++ src/components/Carrinho/CarrinhoMyllena.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class CarrinhoMyllena extends Component {
+  render() {
+    return (
+      <div>CarrinhoMyllena</div>
+    )
+  }
+}
---
+++ src/components/MainContainer/SectionCarrinho.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SectionCarrinho() {
+  return (
+    <div>SectionCarrinho</div>
+  )
+}
